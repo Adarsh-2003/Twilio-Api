be1437d24a8025f910261aa62cf7de2e1b503f8d
Adarsh Gupta <adgupta183@gmail.com>
2023-09-02 23:29:10 +0530

    Update sendSms.py

--- sendSms.py
+++ sendSms.py
@@ -1,6 +1,6 @@
 from twilio.rest import Client
-acc = 'AC835f868c368001b858f11dc6a999c544'
-tok = '40c29c764ee9ce2f2af7e6dbf525e014'
+acc = 'YourTwilioAccess'
+tok = 'YourTwilioToken'
 client = Client(acc,tok)
 msg = client.api.account.messages.create(
      body="I'm ADARSH please CALL me immediately its EMERGENCY",
