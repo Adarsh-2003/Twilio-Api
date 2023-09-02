1991eed629b3e4eb3350feb2d30ec4c1eccdc89e
Adarsh Gupta <adgupta183@gmail.com>
2023-09-02 23:28:27 +0530

    Update callMsg.py

--- callMsg.py
+++ callMsg.py
@@ -1,6 +1,6 @@
 from twilio.rest import Client
-acc = 'AC835f868c368001b858f11dc6a999c544'
-tok = '40c29c764ee9ce2f2af7e6dbf525e014'
+acc = 'YourTwilioAccess'
+tok = 'YourTwilioToken'
 client = Client(acc,tok)
 call = client.calls.create(
      twiml='<Response><Say>Hello Adarsh, this is luffy i just wanna say i am gonna be king of pirates</Say></Response>',
